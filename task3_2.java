c96d0d8fb873c483eecde92147471c422bb8923a
lenschik.kor <lenschik.kor@yandex.ru>
2023-05-12 15:30:49 +0300

    задача3_2

---
+++ task3_2.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+//* Пусть дан произвольный список целых чисел, удалить из него четные числа
+
+public class task3_2 {
+ public static void main(String[] args) {
+ ArrayList<Integer> intList = new ArrayList<>();
+ randFillList(20, 100, intList);
+
+ printArrayList(", ", intList);
+
+ removeEven(intList);
+ printArrayList(", ", intList);
+    }
+
+ public static void removeEven(ArrayList<Integer> list) {
+ for (int i = 0; i < list.size(); i++) {
+ if (list.get(i) % 2 == 0) {
+ list.remove(i);
+ i--;
+            }
+        }
+    }
+
+ public static void printArrayList(String sep, ArrayList<Integer> list) {
+ for (int i = 0; i < list.size(); i++) {
+ if (i == list.size() - 1) {
+ System.out.println(list.get(i));
+ break;
+            }
+ System.out.print(list.get(i) + sep);
+        }
+    }
+
+ public static void randFillList(int count, int maxValue, ArrayList<Integer> list) {
+ Random rand = new Random();
+ for (int i = 0; i < count; i++) list.add(rand.nextInt(maxValue));
+    }
+}
+
